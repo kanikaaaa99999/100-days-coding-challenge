2195705d7c78c8f13559c922757df267152f295d
kanikaaaa99999 <kooslovv@gmail.com>
2025-09-05 00:54:30 +0530

    Add files via upload

---
+++ Q10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int seconds, hours, minutes, secs;
+    printf("Enter time in seconds: ");
+    scanf("%d", &seconds);
+    hours = seconds / 3600;
+    minutes = (seconds % 3600) / 60;
+    secs = seconds % 60;
+    printf("%d:%d:%d\n", hours, minutes, secs);
+    return 0;
+}
---
+++ Q10output.c
@@ -0,0 +1,4 @@
+Enter time in seconds: 3661
+1:1:1 
+Enter time in seconds: 7322
+2:2:2
