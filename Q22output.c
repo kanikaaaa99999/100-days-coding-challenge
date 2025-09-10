9e05a80530005744880f7f387e27042500e525b0
kanikaaaa99999 <kooslovv@gmail.com>
2025-09-10 20:13:44 +0530

    Add files via upload

---
+++ Q22output.c
@@ -0,0 +1,10 @@
+Enter cost price: 1000 1200 
+Enter selling price: Profit: 200.00
+Profit Percentage: 20.00%
+
+Enter cost price: 1000 800
+Enter selling price: Loss: 200.00
+Loss Percentage: 20.00%
+ 
+Enter cost price: 1000 1000
+Enter selling price: No profit, no loss.
