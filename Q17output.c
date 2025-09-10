413eedeecf83685e9e02029d8d8627ee7e463ed7
kanikaaaa99999 <kooslovv@gmail.com>
2025-09-10 20:05:41 +0530

    Add files via upload

---
+++ Q17output.c
@@ -0,0 +1,8 @@
+Enter coefficients a, b and c: 1 -3 2
+Roots are real and different.
+Root 1 = 2.00
+Root 2 = 1.00  
+Enter coefficients a, b and c: 1 2 5
+Roots are complex and different.
+Root 1 = -1.00 + 2.00i
+Root 2 = -1.00 - 2.00i
