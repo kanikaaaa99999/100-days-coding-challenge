30f9fe3a69b40b23d434275260e500759434300a
kanikaaaa99999 <kooslovv@gmail.com>
2025-09-11 10:42:58 +0530

    Add files via upload

---
+++ Q31.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    if (num < 0)
+    {
+        printf("Please enter a positive integer.");
+    }
+    else
+    {
+        int binary[32];
+        int index = 0;
+        while (num > 0)
+        {
+            binary[index] = num % 2;
+            num = num / 2;
+            index++;
+        }
+        printf("Binary representation: ");
+        for (int i = index - 1; i >= 0; i--)
+        {
+            printf("%d", binary[i]);
+        }
+        printf("\n");
+        }
+    return 0;
+}
---
+++ Q31output.c
@@ -0,0 +1,4 @@
+Enter a positive integer: 10
+Binary representation: 1010
+Enter a positive integer: 7
+Binary representation: 111
